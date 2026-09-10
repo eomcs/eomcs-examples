ea2f3c7c2c2a30908fe8e08fa202885801a716a8
eomjinyoung <jinyoung.eom@gmail.com>
2026-09-10 15:07:07 +0900

    .

---
+++ eomcs-web/javascript-lab/todo.js
@@ -0,0 +1,37 @@
+"use strict"
+
+console.log("할 일 목록 시작!");
+
+let workingLength = 15;
+let completedLength = 60;
+
+const workingLengthTag = document.getElementById('working-length');
+const completedLengthTag = document.getElementById('completed-length');
+const workingListTag = document.getElementById('working-list');
+const completedListTag = document.getElementById('completed-list');
+
+workingLengthTag.innerHTML = workingLength;
+completedLengthTag.innerHTML = completedLength;
+
+
+const todoItems = [
+    {
+        workId: 100, 
+        title: "객체 만들기"
+    },
+    {
+        workId: 101, 
+        title: "배열 만들기"
+    }
+];
+
+const todoItemTag = `<article>
+  <input id="work${todoItem.workId}" type="checkbox">
+  <label for="work${todoItem.workId}">${todoItem.title}</label>
+</article>`;
+
+console.log(todoItemTag);
+
+workingListTag.innerHTML = workingListTag.innerHTML + todoItemTag;
+
+
